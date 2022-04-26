2753e9aa6d5aa8ab5e2743e0d9d9e34af93cf143
Andres Gutierrez <andres9888@gmail.com>
2022-04-25 20:30:42 -0400

    intitial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import * as React from "react";
 import * as API from "./api";
 
 export default function App() {
+  const userDetails = API.getUserDetails();
+  console.log("🚀 ~ file: App.js ~ line 6 ~ App ~ userDetails", userDetails)
+  
   return (
     <div>
       <h1>Your stuff here.</h1>
